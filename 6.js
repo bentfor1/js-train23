0eafb9029b244573289dc97564ada5fbb3320fda
bentfor1 <vasyl.pidoyma@gmail.com>
2023-07-18 16:56:45 +0300

    my homework

--- 6.js
+++ 6.js
@@ -2,43 +2,48 @@
 
 // Клас Basket представляє кошик для покупок з певною стратегією знижки
 class Basket {
-  // Створимо конструктор приймає, що стратегію знижки discountPlan як параметр
-  // Властивість discountPlan отримує значення стратегії знижки, яке було передано конструктору
-  // Створюємо новий пустий масив для зберігання товарів (goods) в кошику
-  // Робимо метод addGood, що приймає один параметр - good, який потрібно додати до масиву
-  // Додаємо новий товар в масив товарів
-  // Робимо метод calculateTotalPrice, що розраховує загальну вартість товарів в кошику з урахуванням знижки
-  // За допомогою метода reduce ми сумуємо вартість всіх товарів в масиві
-  // Застосовуємо знижку до загальної вартості за допомогою метода applyDiscount нашого об'єкта discountPlan
+  constructor(discountPlan) {
+    this.discountPlan = discountPlan; // Зберігаємо стратегію знижки
+    this.goods = []; // Створюємо пустий масив для зберігання товарів в кошику
+  }
+
+  addGood(good) {
+    this.goods.push(good); // Додаємо товар в масив товарів
+  }
+
+  calculateTotalPrice() {
+    const totalPrice = this.goods.reduce((sum, good) => sum + good.price, 0); // Розраховуємо загальну вартість товарів
+    return this.discountPlan.applyDiscount(totalPrice); // Застосовуємо знижку до загальної вартості
+  }
 }
 
-// Клас RegularDiscountPlan: стратегія знижки для постійних клієнтів
-class RegularDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 10% price * 0.9
+class RegularDiscountPlan {
+  applyDiscount(price) {
+    return price * 0.9; // Знижка в 10%
+  }
 }
 
-//Клас VIPDiscountPlan: стратегія знижки для VIP клієнтів
-class VIPDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 20% price * 0.8
+class VIPDiscountPlan {
+  applyDiscount(price) {
+    return price * 0.8; // Знижка в 20%
+  }
 }
 
-// Клас NewClientDiscountPlan: стратегія знижки для нових клієнтів
-class NewClientDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 5% price * 0.95
+class NewClientDiscountPlan {
+  applyDiscount(price) {
+    return price * 0.95; // Знижка в 5%
+  }
 }
 
 console.log("Завдання 6 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створення нового екземпляру кошика зі стратегією знижки для нових клієнтів
-// const basket1 = new Basket(new NewClientDiscountPlan());
+const basket1 = new Basket(new NewClientDiscountPlan());
 
 // Додавання товарів до кошика
-// basket1.addGood({ name: "Product 1", price: 100 });
-// basket1.addGood({ name: "Product 2", price: 50 });
+basket1.addGood({ name: "Product 1", price: 100 });
+basket1.addGood({ name: "Product 2", price: 50 });
 
 // Розрахунок і виведення загальної вартості товарів з урахуванням знижки
-// console.log(basket1.calculateTotalPrice());
+console.log(basket1.calculateTotalPrice());
